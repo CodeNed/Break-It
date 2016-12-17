9e31ef5c8b629a3b2839f39169cb8fe0512f344f
DayTripperID <noahdering@gmail.com>
2016-12-17 16:16:42 -0500

    Added some new keys.

--- src/com/noah/breakit/input/Keyboard.java
+++ src/com/noah/breakit/input/Keyboard.java
@@ -7,15 +7,26 @@ public class Keyboard implements KeyListener {
 
 	public boolean[] keys = new boolean[120];
 
-	public boolean up, down, left, right, enter, space;
+	public boolean up, down, left, right, enter, space, esc;
+	public boolean upLast, downLast, leftLast, rightLast, enterLast, spaceLast, escLast;
 
 	public void update() {
-		up = keys[KeyEvent.VK_W] || keys[KeyEvent.VK_UP];
-		down = keys[KeyEvent.VK_S] || keys[KeyEvent.VK_DOWN];
-		left = keys[KeyEvent.VK_A] || keys[KeyEvent.VK_LEFT];
+		
+		upLast    = up;
+		downLast  = down;
+		leftLast  = left;
+		rightLast = right;
+		enterLast = enter;
+		spaceLast = space;
+		escLast   = esc;
+		
+		up    = keys[KeyEvent.VK_W] || keys[KeyEvent.VK_UP];
+		down  = keys[KeyEvent.VK_S] || keys[KeyEvent.VK_DOWN];
+		left  = keys[KeyEvent.VK_A] || keys[KeyEvent.VK_LEFT];
 		right = keys[KeyEvent.VK_D] || keys[KeyEvent.VK_RIGHT];
 		enter = keys[KeyEvent.VK_ENTER];
 		space = keys[KeyEvent.VK_SPACE];
+		esc   = keys[KeyEvent.VK_ESCAPE];
 	}
 
 	public void keyTyped(KeyEvent e) {
